1a295c79e41172a694e510a964af46a0759fabfd
Chris Jones <chris@jonezy.org>
2013-06-16 10:06:27 -0400

    this version is done.
    have to figure out how I intend to use the data in
    findttc before going any further
    
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- test/cli-tests.js
+++ test/cli-tests.js
@@ -2,8 +2,8 @@ var assert = require('assert');
 var trup  = require('../bin/trup');
 
 describe("trup cli", function() {
-  it("it should have an update command", function() {
-   var expected = "update"; 
+  it("it should have a run command", function() {
+   var expected = "run"; 
    var actual = "";
    for(var i = 0; i < trup.commands.length;i++) {
      if(trup.commands[i].name === expected)
